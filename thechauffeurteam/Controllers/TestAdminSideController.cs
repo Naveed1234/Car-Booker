a7cbac232daca8692a526cbcedf1884d7962247a
malik <malik@DESKTOP-U7IP22V>
2019-05-08 22:02:42 -0700

    Add project files.

---
+++ thechauffeurteam/Controllers/TestAdminSideController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using thechauffeurteam.DAL;
+
+namespace thechauffeurteam.Controllers
+{
+    public class TestAdminSideController : Controller
+    {
+        private MyContext db = new MyContext();
+        // GET: TestAdminSide
+        public ActionResult BokeJob()
+        {
+            ViewBag.pickUpPostcode = new SelectList(db.PostCodes.ToList(), "Id", "PostCodeValue");
+            ViewBag.dropOffPostcode = new SelectList(db.PostCodes.ToList(), "Id", "PostCodeValue");
+
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Scroll()
+        {
+            return View();
+        }
+
+
+        public ActionResult BookingJob()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ thechauffeurteam/Models/job.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace thechauffeurteam.Models
+{
+    public class job
+    {
+
+        public int id { get; set; }
+
+        public int? PassengerId { get; set; }
+
+        public Passenger passenger { get; set; }
+
+        public string PassengerName { get; set; }
+        public string PassengerPhone { get; set; }
+
+
+
+        public string DriverId { get; set; }
+        public string DriverName { get; set; }
+        public string dateAndTime { get; set; }
+        public string pickUp { get; set; }
+
+        public string DropUP { get; set; }
+
+        public string CarType { get; set; }
+
+        public string JobType { get; set; }
+
+        public string Price { get; set; }
+
+        public string DriverMessage { get; set; }
+
+        public int? status { get; set; }
+
+        public int? Hours { get; set; }
+
+        public int? Mile { get; set; }
+
+
+
+
+    }
+}
